693187adbf987db526fe20771c7f8b63bddd65f9
Denis Monteiro <deniswmonteiro@gmail.com>
2023-06-18 19:52:44 -0300

    Final adjusts

--- js/review.js
+++ js/review.js
@@ -3,14 +3,39 @@ window.leaveAReview = function (el) {
     const selectedStar = $(el).attr("id").split("-")[3];
     const icons = document.querySelectorAll("[data-review-star]");
 
-    // Activate selected star and the previous
-    for (let i = selectedStar; i > 0; i--) {
-        if (icons[i - 1].classList.contains("active")) $(`#${icons[i - 1].id} .add-star`)[0].beginElement();
+    if ($(el).hasClass("selected")) {
+        const iconsSelected = document.querySelectorAll("[data-review-star].selected");
+        const lastIconSelected = $.map(iconsSelected, function (icon) {
+            return $(icon).attr("id").split("-")[3];
+        });
+
+        // Desactivate the star and the previous
+        if ($(el).attr("id").split("-")[3] === lastIconSelected[lastIconSelected.length - 1]) {
+            for (let i = selectedStar; i > 0; i--) {
+                $(`#${icons[i - 1].id} .remove-star`)[0].beginElement();
+                $(`#${icons[i - 1].id}`).removeClass("selected");
+            }
+        }
+
+        // Desactivate the next stars from selected
+        for (let i = selectedStar; i < icons.length; i++) {
+            $(`#${icons[i].id} .remove-star`)[0].beginElement();
+            $(`#${icons[i].id}`).removeClass("selected");
+        }
     }
 
-    // Desactivate the next stars from selected
-    for (let i = selectedStar; i < icons.length; i++) {
-        if (!icons[i].classList.contains("active")) $(`#${icons[i].id} .remove-star`)[0].beginElement();
+    else {
+        // Activate selected star and the previous
+        for (let i = selectedStar; i > 0; i--) {
+            $(`#${icons[i - 1].id} .add-star`)[0].beginElement();
+            $(`#${icons[i - 1].id}`).addClass("selected");
+        }
+
+        // Desactivate the next stars from selected
+        for (let i = selectedStar; i < icons.length; i++) {
+            $(`#${icons[i].id} .remove-star`)[0].beginElement();
+            $(`#${icons[i].id}`).removeClass("selected");
+        }
     }
 }
 
@@ -21,7 +46,7 @@ window.starHoverActive = function (el) {
 
     // Activate selected star and the previous
     for (let i = selectedStar; i > 0; i--) {
-        $(`#${icons[i - 1].id}`).addClass("active");
+        $(`#${icons[i - 1].id}`).addClass("activeHover");
     }
 }
 
@@ -32,6 +57,6 @@ window.starHoverInactive = function (el) {
 
     // Activate selected star and the previous
     for (let i = selectedStar; i > 0; i--) {
-        $(`#${icons[i - 1].id}`).removeClass("active");
+        $(`#${icons[i - 1].id}`).removeClass("activeHover");
     }
 }
