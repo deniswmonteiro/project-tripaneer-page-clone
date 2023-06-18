749b7ec81659f39f71fd9cdd13ddb2b7d92d60ed
Denis Monteiro <deniswmonteiro@gmail.com>
2023-06-18 17:21:57 -0300

    Subscribe section creation

--- js/review.js
+++ js/review.js
@@ -5,11 +5,33 @@ window.leaveAReview = function (el) {
 
     // Activate selected star and the previous
     for (let i = selectedStar; i > 0; i--) {
-        $(`#${icons[i - 1].id} .add-star`)[0].beginElement()
+        if (icons[i - 1].classList.contains("active")) $(`#${icons[i - 1].id} .add-star`)[0].beginElement();
     }
 
     // Desactivate the next stars from selected
     for (let i = selectedStar; i < icons.length; i++) {
-        $(`#${icons[i].id} .remove-star`)[0].beginElement()
+        if (!icons[i].classList.contains("active")) $(`#${icons[i].id} .remove-star`)[0].beginElement();
+    }
+}
+
+/** Activate star when mouse over */
+window.starHoverActive = function (el) {
+    const selectedStar = $(el).attr("id").split("-")[3];
+    const icons = document.querySelectorAll("[data-review-star]");
+
+    // Activate selected star and the previous
+    for (let i = selectedStar; i > 0; i--) {
+        $(`#${icons[i - 1].id}`).addClass("active");
+    }
+}
+
+/** Desactivate star when mouse leave */
+window.starHoverInactive = function (el) {
+    const selectedStar = $(el).attr("id").split("-")[3];
+    const icons = document.querySelectorAll("[data-review-star]");
+
+    // Activate selected star and the previous
+    for (let i = selectedStar; i > 0; i--) {
+        $(`#${icons[i - 1].id}`).removeClass("active");
     }
 }
